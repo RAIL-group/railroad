6f6691702ac52bc0501e8b3b9833f1852523a3a4
Gregory Stein <gregory.j.stein@gmail.com>
2025-08-02 11:38:08 -0400

    clean up test_cpp, removing all 'temp' classes no longer needed

--- mrppddl/tests/test_cpp.py
+++ mrppddl/tests/test_cpp.py
@@ -1,5 +1,7 @@
 import pytest
-from mrppddl._bindings import GroundedEffect, Fluent, Action, State, transition
+from mrppddl.core import GroundedEffect, Fluent, Action, State, transition, get_action_by_name
+from mrppddl.helper import construct_move_operator, construct_move_visited_operator
+import random
 
 F = Fluent
 
@@ -106,7 +108,7 @@ def test_cpp_action_instantiation():
     assert s.satisfies_precondition(a1)
     assert not s.satisfies_precondition(a2)
 
-    out = transition(s, a1)
+    transition(s, a1)  # Confirm this runs
 
 def test_state_effects_pop_order_correct():
     state = State(fluents={Fluent("free robot")})
@@ -126,124 +128,8 @@ def test_state_effects_pop_order_correct():
 
 # DEBUG
 
-import itertools
 from typing import List, Tuple, Sequence, Dict, Set
-from mrppddl._bindings import get_next_actions, astar, MCTSPlanner, make_goal_test
-from mrppddl.core import _make_bindable, OptExpr, Binding
-from mrppddl.helper import _make_callable, OptCallable
-
-class LiftedEffectType:
-    def __init__(self, time: OptExpr, resulting_fluents: Set[Fluent]):
-        self.time = _make_bindable(time)
-        self.resulting_fluents = resulting_fluents
-
-    def _ground(self, binding: Binding) -> "GroundedEffect":
-        grounded_time = self.time(binding)
-        grounded_fluents = frozenset(
-            Fluent(
-                f.name, *[binding.get(arg, arg) for arg in f.args], negated=f.negated
-            )
-            for f in self.resulting_fluents
-        )
-        return GroundedEffect(grounded_time, grounded_fluents)
-
-class Effect(LiftedEffectType):
-    def __init__(self, time: OptExpr, resulting_fluents: Set[Fluent]):
-        self.time = _make_bindable(time)
-        self.resulting_fluents = resulting_fluents
-
-class ProbEffect(LiftedEffectType):
-    def __init__(
-        self,
-        time: OptExpr,
-        prob_effects: List[Tuple[OptExpr, List[Effect]]],
-        resulting_fluents: Set[Fluent] = set(),
-    ):
-        self.time = _make_bindable(time)
-        self.prob_effects = [
-            (_make_bindable(prob), effects) for prob, effects in prob_effects
-        ]
-        self.resulting_fluents = resulting_fluents
-
-    def _ground(self, binding: Binding) -> "GroundedEffect":
-        grounded_prob_effects = tuple(
-            (prob(binding), tuple(e._ground(binding) for e in effect_list))
-            for prob, effect_list in self.prob_effects
-        )
-
-        grounded_time: float = self.time(binding)
-        grounded_resulting_fluents = frozenset(
-            Fluent(
-                f.name, *[binding.get(arg, arg) for arg in f.args], negated=f.negated
-            )
-            for f in self.resulting_fluents
-        )
-
-        return GroundedEffect(
-            grounded_time, grounded_resulting_fluents, grounded_prob_effects
-        )
-
-class OperatorCpp:
-    def __init__(
-        self,
-        name: str,
-        parameters: List[Tuple[str, str]],
-        preconditions: List[Fluent],
-        effects: Sequence[LiftedEffectType],
-    ):
-        self.name = name
-        self.parameters = parameters
-        self.preconditions = preconditions
-        self.effects = effects
-
-    def instantiate(self, objects_by_type: Dict[str, List[str]]) -> List[Action]:
-        grounded_actions = []
-        domains = [objects_by_type[typ] for _, typ in self.parameters]
-        for assignment in itertools.product(*domains):
-            binding = {var: obj for (var, _), obj in zip(self.parameters, assignment)}
-            if len(set(binding.values())) != len(binding):
-                continue
-            grounded_actions.append(self._ground(binding))
-        return grounded_actions
-
-    def _ground(self, binding: Dict[str, str]) -> Action:
-        def evaluate(value):
-            return value(binding) if callable(value) else value
-
-        grounded_preconditions = frozenset(
-            self._substitute_fluent(f, binding) for f in self.preconditions
-        )
-        grounded_effects = [eff._ground(binding) for eff in self.effects]
-
-        name_str = f"{self.name} " + " ".join(
-            binding[var] for var, _ in self.parameters
-        )
-        return Action(grounded_preconditions, grounded_effects, name=name_str)
-
-    def _substitute_fluent(self, fluent: Fluent, binding: Dict[str, str]) -> Fluent:
-        grounded_args = tuple(binding.get(arg, arg) for arg in fluent.args)
-        return Fluent(fluent.name, *grounded_args, negated=fluent.negated)
-
-
-def construct_move_operator(move_time: OptCallable):
-    move_time = _make_callable(move_time)
-    return OperatorCpp(
-        name="move_visit",
-        parameters=[("?r", "robot"), ("?from", "location"), ("?to", "location")],
-        preconditions=[F("at ?r ?from"), F("free ?r"), F("not visited ?to")],
-        effects=[
-            Effect(time=0, resulting_fluents={F("not free ?r"), F("not at ?r ?from")}),
-            Effect(time=(move_time, ["?r", "?from", "?to"]),
-                   resulting_fluents={F("free ?r"), F("at ?r ?to"), F("visited ?to")},
-            ),
-        ],
-    )
-
-def get_action_by_name(actions: List[Action], name: str) -> Action:
-    for action in actions:
-        if action.name == name:
-            return action
-    raise ValueError(f"No action found with name: {name}")
+from mrppddl._bindings import astar, MCTSPlanner, make_goal_test
 
 def test_cpp_deepcopy():
     import copy
@@ -261,17 +147,15 @@ def test_cpp_deepcopy():
     ge_alt = GroundedEffect(3.0, {Fluent("at robot cabinet")})
     ge_prob = GroundedEffect(3.0, prob_effects=[(0.4, [ge]), (0.6, [ge_alt])])
     ge_prob_copy = copy.deepcopy(ge_prob)
-    print(ge_prob)
-    print(ge_prob_copy)
     assert hash(ge_prob) == hash(ge_prob_copy)
 
     # Get all actions
     objects_by_type = {
         "robot": ["r1", "r2"],
-        "location": ["start", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
-        # "location": ["start", "a", "b"],
+        "location": ["start", "a", "b", "c",
+                     "d", "e", "f", "g", "h",
+                     "i", "j", "k"],
     }
-    import random
     random.seed(8616)
     move_op = construct_move_operator(lambda *args: 5.0 + random.random())
     all_actions = move_op.instantiate(objects_by_type)
@@ -290,86 +174,56 @@ def test_cpp_deepcopy():
     initial_state = copy.deepcopy(initial_state)
 
 
-@pytest.mark.skip()
-def test_cpp_astar_move():
-
+@pytest.mark.parametrize(
+    "initial_fluents", [
+        {F("at r1 start"), F("free r1"),
+         F("visited start")},
+        {F("at r1 start"), F("free r1"),
+         F("at r2 start"), F("free r2"),
+         F("visited start")},
+        {F("at r1 start"), F("free r1"),
+         F("at r2 start"), F("free r2"),
+         F("at r3 start"), F("free r3"),
+         F("visited start")},
+    ],
+    ids=["one robot", "two robots", "three robots"])
+def test_planner_mcts_move_visit_multirobot(initial_fluents):
     # Get all actions
     objects_by_type = {
-        "robot": ["r1", "r2"],
-        "location": ["start", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k"],
-        # "location": ["start", "a", "b"],
+        "robot": ["r1", "r2", "r3"],
+        "location": ["start", "a", "b", "c",
+                     "d", "e", "f", "g", "h",
+                     "i", "j", "k"],
     }
-    import random
     random.seed(8616)
-    move_op = construct_move_operator(lambda *args: 5.0 + random.random())
+    move_op = construct_move_visited_operator(lambda *args: 5.0 + random.random())
     all_actions = move_op.instantiate(objects_by_type)
 
     # Initial state
     initial_state = State(
         time=0,
-        fluents={
-            F("at r1 start"), F("free r1"),
-            F("at r2 start"), F("free r2"),
-            F("visited start"),
-        })
-    # s = initial_state
-    # s = transition(s, get_action_by_name(all_actions, "move r1 start a"))[0][0]
-    # print(s)
-    # s = transition(s, get_action_by_name(all_actions, "move r2 start b"))[0][0]
-    # print(s)
-    # s = transition(s, get_action_by_name(all_actions, "move r2 b c"))[0][0]
-    # print(s)
-    goal_fn = make_goal_test({
-        F("at r1 a"),
-        F("visited a"), 
-        F("visited b"),
-        F("visited c"),
-        F("visited d"),
-        # F("visited e"),
-    })
+        fluents=initial_fluents)
     goal_fluents = {
-        # F("at r1 a"),
         F("visited a"),
         F("visited b"),
         F("visited c"),
         F("visited d"),
         F("visited e"),
-        F("visited f"),
-        F("visited g"),
     }
-    # from mrppddl.planner import astar
-    # def goal_fn(fluents):
-    #     return F("at r1 a") in fluents
     def is_goal(state):
         return all(gf in state.fluents
                    for gf in goal_fluents)
-    from time import time
-    tstart = time()
     state = initial_state
     mcts = MCTSPlanner(all_actions)
     for _ in range(15):
         if is_goal(state):
             print("Goal found!")
             break
-        action_name = mcts(state, goal_fluents, 100000, c=10)
+        action_name = mcts(state, goal_fluents, 10000, c=10)
         if action_name == "NONE":
             break
         action = get_action_by_name(all_actions, action_name)
 
         state = transition(state, action)[0][0]
         print(action_name, state, is_goal(state))
-    #     # action = mcts(state, all_actions, goal_fluents)
-
-    # # # action_name = mcts(state, all_actions, goal_fluents, 1000)
-    print(path)
-    path = astar(initial_state, all_actions, goal_fluents)
-    print(time() - tstart)
-    s = initial_state
-    for action in path:
-        print(s)
-        print(action.name)
-        assert s.satisfies_precondition(action)
-        s = transition(s, action)[0][0]
-    print("State")
-    print(s)
-    raise ValueError()
+    assert is_goal(state)
