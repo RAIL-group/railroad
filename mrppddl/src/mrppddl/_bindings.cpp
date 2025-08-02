b4139a4092ab5be48de036d8b4c0178e2cc9eedf
Gregory Stein <gregory.j.stein@gmail.com>
2025-08-02 13:16:37 -0400

    rm some commented out code in bindings

--- mrppddl/src/mrppddl/_bindings.cpp
+++ mrppddl/src/mrppddl/_bindings.cpp
@@ -291,12 +291,6 @@ PYBIND11_MODULE(_bindings, m) {
   m.def("astar", &astar, py::arg("start_state"), py::arg("all_actions"),
         py::arg("is_goal_state"), py::arg("heuristic_fn") = nullptr,
         "Run A* search and return the action path");
-  // m.def("mcts", &mcts, py::arg("start_state"), py::arg("all_actions"),
-  //       py::arg("is_goal_state"),
-  // 	py::arg("max_iterations") = 1000,
-  // 	py::arg("max_depth") = 20,
-  // 	py::arg("c") = 1.414,
-  //       "Run A* search and return the action path");
   py::class_<MCTSPlanner>(m, "MCTSPlanner")
     .def(py::init<std::vector<Action>>(), py::arg("all_actions"))
     .def("__call__",
@@ -313,8 +307,4 @@ PYBIND11_MODULE(_bindings, m) {
 	 py::arg("max_iterations") = 1000,
 	 py::arg("max_depth") = 20,
 	 py::arg("c") = 1.414);
-
-  // py::bind_vector<std::vector<GroundedEffect>>(m, "GroundedEffectVec");
-  // py::bind_vector<std::vector<ProbBranch>>(m, "GroundedProbBranchVec");
-  // py::bind_tuple<ProbBranch>(m, "ProbBranch");
 }
