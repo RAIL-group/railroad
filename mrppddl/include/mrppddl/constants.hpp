77adfb08d5fb269000a3ca5ca2d988b4473dfb22
Gregory Stein <gregory.j.stein@gmail.com>
2025-11-21 13:36:56 -0800

    increase fail penalty by 100x; resolves looping behavior
    Apparently the system decided it was cheaper to not succeed than it was to try
    to finish the task. Not totally sure how it came to that conclusion, but that
    seemed to have been the issue. I wonder if increasing the planning depth was
    another way to address this problem, since the net cost would have been greater.
    Still, this is worthy of further study, as playing around with this value in
    great detail isn't likely a good long-term solution.

--- mrppddl/include/mrppddl/constants.hpp
+++ mrppddl/include/mrppddl/constants.hpp
@@ -2,7 +2,7 @@
 
 namespace mrppddl {
 
-const double HEURISTIC_CANNOT_FIND_GOAL_PENALTY = 10.0;
+const double HEURISTIC_CANNOT_FIND_GOAL_PENALTY = 1000.0;
 const double ALL_ROBOTS_WAITING_PENALTY = 10.0;
 
 }
