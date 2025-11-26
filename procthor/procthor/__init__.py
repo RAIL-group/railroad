2824effd289e55d92ac5bf5658d06eaf216377f9
Gregory Stein <gregory.j.stein@gmail.com>
2025-11-26 11:37:28 -0500

    rm old 'procthor.py'; replace with what used to be 'procthor_new.py'

--- procthor/procthor/__init__.py
+++ procthor/procthor/__init__.py
@@ -1,4 +1,4 @@
-from .procthor_new import ThorInterface  # noqa: F401
+from .procthor import ThorInterface  # noqa: F401
 from . import simulators  # noqa: F401
 from . import plotting  # noqa: F401
 from . import utils  # noqa: F401
--- procthor/procthor/procthor.py
+++ procthor/procthor/procthor.py
@@ -1,11 +1,14 @@
 import json
-import copy
 import numpy as np
+import random
+import copy
 from shapely import geometry
+from common import Pose
 from ai2thor.controller import Controller
-
-import procthor
 from . import utils
+from .scenegraph import SceneGraph
+import pickle
+from pathlib import Path
 
 IGNORE_CONTAINERS = [
     'baseballbat', 'basketBall', 'boots', 'desklamp', 'painting',
@@ -16,11 +19,12 @@ IGNORE_CONTAINERS = [
 
 
 class ThorInterface:
-    def __init__(self, args, preprocess=True):
+    def __init__(self, args, preprocess=True, use_cache=True):
         self.args = args
         self.seed = args.current_seed
-        self.grid_resolution = args.resolution
+        random.seed(self.seed)
 
+        self.grid_resolution = args.resolution
         self.scene = self.load_scene()
 
         self.rooms = self.scene['rooms']
@@ -46,48 +50,50 @@ class ThorInterface:
                 if container['id'].split('|')[0].lower() not in IGNORE_CONTAINERS
             ]
 
-        self.cache = None
-        if hasattr(args, "cache_path") and args.cache_path:
-            self.cache = procthor.utils.load_cache(self.seed, args.cache_path)
-        if self.cache is None:
+        self.cached_data = self.load_cache() if use_cache else None
+        if self.cached_data is None:
             self.controller = Controller(scene=self.scene,
                                          gridSize=self.grid_resolution,
                                          width=480, height=480)
+            self.cached_data = self.save_and_get_cache()
+        else:
+            print("-----------Using cached procthor data-----------")
+            self.controller = None
+
         self.occupancy_grid = self.get_occupancy_grid()
-
-        if type(preprocess) is dict:
-            # add custom objects in their prefered locations; todo so,
-            # we first check if necessary container exists, create an
-            # eligible pool dictionary then randomly choose 1 or 40% from
-            # the pool, whichever max and add it to those containers
-            eligible_pool = {}
-            for container in self.containers:
-                cnt_ID = container['id'].split('|')[0].lower()
-                for obj in preprocess:
-                    if cnt_ID in preprocess[obj]:
-                        if obj not in eligible_pool:
-                            eligible_pool[obj] = [container['id']]
-                        else:
-                            eligible_pool[obj].append(container['id'])
-            chosen_containers = {}
-            for obj in eligible_pool:
-                choice_count = max(1, int(0.4 * len(eligible_pool[obj])))
-                chosen_containers[obj] = np.random.choice(eligible_pool[obj], choice_count)
-
-            counter = 0
-            for container in self.containers:
-                for obj in chosen_containers:
-                    if container['id'] in chosen_containers[obj]:
-                        counter += 1
-                        if 'children' not in container:
-                            container['children'] = []
-                        container['children'].append({
-                            'id': f'{obj}|surface|{counter}',
-                            'kinematic': False,
-                            'position': container['position']
-                        })
-
+        self.scene_graph = self.get_scene_graph()
+        self.robot_pose = self.get_robot_pose()
         self.known_cost = self.get_known_costs()
+
+    def save_and_get_cache(self, path='./resources/procthor-10k/cache'):
+        """Cache the scene data."""
+        cache = {
+            'reachable_positions': self.get_reachable_positions(),
+            'image_ortho': self.get_top_down_image(orthographic=True),
+            'image_persp': self.get_top_down_image(orthographic=False)
+        }
+        save_dir = Path(path)
+        save_dir.mkdir(parents=True, exist_ok=True)
+        with open(save_dir / f'scene_{self.args.current_seed}.pkl', 'wb') as f:
+            pickle.dump(cache, f)
+        return cache
+
+    def load_cache(self, path='./resources/procthor-10k/cache'):
+        """Load the cached scene data."""
+        cache_file = Path(path) / f'scene_{self.args.current_seed}.pkl'
+        if not cache_file.exists():
+            return None
+        with open(cache_file, 'rb') as f:
+            cache = pickle.load(f)
+        return cache
+
+    def gen_map_and_poses(self, num_objects=1):
+        """Returns scene graph, grid, initial robot pose and target object info."""
+        self.target_objs_info = self.get_target_objs_info(num_objects)
+        return (self.scene_graph,
+                self.occupancy_grid,
+                self.robot_pose,
+                self.target_objs_info)
 
     def load_scene(self, path='./resources/procthor-10k'):
         with open(
@@ -108,25 +114,48 @@ class ThorInterface:
     def get_robot_pose(self):
         position = self.agent['position']
         position = np.array([position['x'], position['z']])
-        return self.scale_to_grid(position)
+        pose = self.scale_to_grid(position)
+        return Pose(*pose)
+
+    def get_target_objs_info(self, num_objects=1):
+        object_name_to_idxs = {}
+        for idx in self.scene_graph.object_indices:
+            name = self.scene_graph.get_node_name_by_idx(idx)
+            if name not in object_name_to_idxs.keys():
+                object_name_to_idxs[name] = [idx]
+            else:
+                object_name_to_idxs[name].append(idx)
+        if num_objects > len(object_name_to_idxs):
+            num_objects = len(object_name_to_idxs)
+
+        target_obj_names = random.sample(list(object_name_to_idxs.keys()), num_objects)
+        target_objs_info = []
+        for name in target_obj_names:
+            idxs = object_name_to_idxs[name]
+            container_idxs = [self.scene_graph.get_parent_node_idx(idx) for idx in idxs]
+            node_type = self.scene_graph.nodes[idxs[0]]['type']
+            target_objs_info.append({
+                'name': name,
+                'idxs': object_name_to_idxs[name],
+                'type': node_type,
+                'container_idxs': container_idxs
+            })
+
+        if num_objects == 1:
+            return target_objs_info[0]
+        return target_objs_info
+
+    def get_reachable_positions(self):
+        if self.cached_data is not None:
+            return self.cached_data['reachable_positions']
+        event = self.controller.step(action="GetReachablePositions")
+        reachable_positions = event.metadata["actionReturn"]
+        return reachable_positions
 
     def get_occupancy_grid(self):
-        if self.cache:
-            RPs = self.cache['RPs']
-        else:
-            event = self.controller.step(action="GetReachablePositions")
-            reachable_positions = event.metadata["actionReturn"]
-            RPs = reachable_positions
-            # save the data in cache as pickle and create necessary directories
-            if hasattr(self.args, "cache_path") and self.args.cache_path:
-                data = {
-                    'RPs': RPs,
-                    'image': self.get_top_down_frame()
-                }
-                procthor.utils.save_cache(self.seed, self.args.cache_path, data)
-
-        xs = [rp["x"] for rp in RPs]
-        zs = [rp["z"] for rp in RPs]
+        rps = self.get_reachable_positions()
+        xs = [rp["x"] for rp in rps]
+        zs = [rp["z"] for rp in rps]
 
         # Calculate the mins and maxs
         min_x, max_x = min(xs), max(xs)
@@ -137,27 +166,13 @@ class ThorInterface:
 
         # Create list of free points
         points = list(zip(xs, zs))
-        grid_to_points_map = {self.scale_to_grid(point): RPs[idx]
+        grid_to_points_map = {self.scale_to_grid(point): rps[idx]
                               for idx, point in enumerate(points)}
         height, width = self.scale_to_grid([max_x, max_z])
         occupancy_grid = np.ones((height + 2, width + 2), dtype=int)
         free_positions = grid_to_points_map.keys()
         for pos in free_positions:
             occupancy_grid[pos] = 0
-
-        min_height, min_width = self.scale_to_grid([min_x, min_z])
-        if height > width:
-            offset_x = - min_height / 2
-            offset_z = (height - width) / 2 - min_width / 2
-            x_max = height
-            z_max = height
-        else:
-            offset_x = (width - height) / 2 - min_height / 2
-            offset_z = - min_width / 2
-            x_max = width
-            z_max = width
-        self.plot_offset = [offset_x, offset_z]
-        self.plot_extent = [0, x_max, z_max, 0]
 
         # store the mapping from grid coordinates to simulator positions
         self.g2p_map = grid_to_points_map
@@ -192,165 +207,83 @@ class ThorInterface:
 
         return occupancy_grid
 
-    def get_top_down_frame(self):
-        if self.cache:
-            return self.cache['image']
-        # Setup the top-down camera
-        event = self.controller.step(action="GetMapViewCameraProperties", raise_for_failure=True)
-        pose = copy.deepcopy(event.metadata["actionReturn"])
+    def get_scene_graph(self):
+        """Create a scene graph from scene data."""
+        graph = SceneGraph()
 
-        bounds = event.metadata["sceneBounds"]["size"]
-        max_bound = max(bounds["x"], bounds["z"])
-
-        pose["fieldOfView"] = 50
-        pose["position"]["y"] += 1.1 * max_bound
-        pose["orthographic"] = True
-        pose["farClippingPlane"] = 50
-        pose["orthographicSize"] = .5 * max_bound
-
-        # add the camera to the scene
-        event = self.controller.step(
-            action="AddThirdPartyCamera",
-            **pose,
-            skyboxColor="white",
-            raise_for_failure=True,
+        # Add apartment node
+        apartment_idx = graph.add_node(
+            {
+                'id': 'Apartment|0',
+                'name': 'apartment',
+                'position': (0, 0),
+                'type': [1, 0, 0, 0]
+            }
         )
-        top_down_frame = event.third_party_camera_frames[-1]
-        top_down_frame = top_down_frame[::-1, ...]
-        return top_down_frame
 
-    def get_graph(self, include_node_embeddings=True):
-        ''' This method creates graph data from the procthor-10k data'''
-
-        # Create dummy apartment node
-        node_count = 0
-        nodes = {}
-        assetId_idx_map = {}
-        edges = []
-        nodes[node_count] = {
-            'id': 'Apartment|0',
-            'name': 'apartment',
-            'pos': (0, 0),
-            'type': [1, 0, 0, 0]
-        }
-        node_count += 1
-
-        # Iterate over rooms but skip position coordinate scaling since not
-        # required in distance calculations
+        # Add room nodes
         for room in self.rooms:
-            nodes[node_count] = {
-                'id': room['id'],
-                'name': room['roomType'].lower(),
-                'pos': room['position'],
-                'type': [0, 1, 0, 0]
-            }
-            edges.append(tuple([0, node_count]))
-            node_count += 1
+            room_idx = graph.add_node(
+                {
+                    'id': room['id'],
+                    'name': room['roomType'].lower(),
+                    'position': room['position'],
+                    'type': [0, 1, 0, 0]
+                }
+            )
+            graph.add_edge(apartment_idx, room_idx)
 
-        # add an edge between two rooms adjacent by a passable shared door
-        room_edges = set()
-        for i in range(1, len(nodes)):
-            for j in range(i + 1, len(nodes)):
-                node_1, node_2 = nodes[i], nodes[j]
-                if utils.has_edge(self.scene['doors'], node_1['id'], node_2['id']):
-                    room_edges.add(tuple(sorted((i, j))))
-        edges.extend(room_edges)
+        # Add edges between connected rooms
+        room_indices = graph.get_node_indices_by_type(1)
+        for i, src_idx in enumerate(room_indices):
+            for dst_idx in room_indices[i + 1:]:
+                src_node = graph.nodes[src_idx]
+                dst_node = graph.nodes[dst_idx]
+                if utils.has_edge(self.scene['doors'], src_node['id'], dst_node['id']):
+                    graph.add_edge(src_idx, dst_idx)
 
-        node_keys = list(nodes.keys())
-        node_ids = [utils.get_room_id(nodes[key]['id']) for key in node_keys]
-        cnt_node_idx = []
-
+        # Add container nodes
         for container in self.containers:
-            cnt_id = utils.get_room_id(container['id'])
-            src = node_ids.index(cnt_id)
-            assetId = container['id']
-            assetId_idx_map[assetId] = node_count
-            name = utils.get_generic_name(container['id'])
-            nodes[node_count] = {
-                'id': container['id'],
-                'name': name,
-                'pos': container['position'],
-                'type': [0, 0, 1, 0]
-            }
-            edges.append(tuple([src, node_count]))
-            cnt_node_idx.append(node_count)
-            node_count += 1
+            room_id = utils.get_room_id(container['id'])
+            room_node_idx = next(idx for idx, node in graph.nodes.items()
+                                 if node['type'][1] == 1 and utils.get_room_id(node['id']) == room_id)
 
-        node_keys = list(nodes.keys())
-        node_ids = [nodes[key]['id'] for key in node_keys]
-        obj_node_idx = []
+            container_idx = graph.add_node(
+                {
+                    'id': container['id'],
+                    'name': utils.get_generic_name(container['id']),
+                    'position': container['position'],
+                    'type': [0, 0, 1, 0]
+                }
+            )
+            # graph.asset_id_to_node_idx_map[container['id']] = container_idx
+            graph.add_edge(room_node_idx, container_idx)
 
+        # Add object nodes for container contents
         for container in self.containers:
             connected_objects = container.get('children')
             if connected_objects is not None:
-                src = node_ids.index(container['id'])
-                for object in connected_objects:
-                    assetId = object['id']
-                    assetId_idx_map[assetId] = node_count
-                    name = utils.get_generic_name(object['id'])
-                    nodes[node_count] = {
-                        'id': object['id'],
-                        'name': name,
-                        'pos': object['position'],
-                        'type': [0, 0, 0, 1]
-                    }
-                    edges.append(tuple([src, node_count]))
-                    obj_node_idx.append(node_count)
-                    node_count += 1
+                container_idx = graph.asset_id_to_node_idx_map[container['id']]
+                for obj in connected_objects:
+                    # graph.asset_id_to_node_idx_map[obj['id']] = len(graph)
+                    obj_idx = graph.add_node(
+                        {
+                            'id': obj['id'],
+                            'name': utils.get_generic_name(obj['id']),
+                            'position': obj['position'],
+                            'type': [0, 0, 0, 1]
+                        }
+                    )
+                    graph.add_edge(container_idx, obj_idx)
 
-        graph = {
-            'nodes': nodes,  # dictionary {id, name, pos, type}
-            'edge_index': edges,  # pairwise edge list
-            'cnt_node_idx': cnt_node_idx,  # indices of contianers
-            'obj_node_idx': obj_node_idx,  # indices of objects
-            'idx_map': assetId_idx_map  # mapping from assedId to graph index position
-        }
-
-        # Add edges to get a connected graph if not already connected
-        req_edges = utils.get_edges_for_connected_graph(self.occupancy_grid, graph, pos='pos')
-        graph['edge_index'] = req_edges + graph['edge_index']
-
-        if not include_node_embeddings:
-            return graph
-
-        # perform some more formatting for the graph, then return
-        node_coords = {}
-        node_names = {}
-        graph_nodes = []
-        node_color_list = []
-
-        for count, node_key in enumerate(graph['nodes']):
-            node_coords[node_key] = graph['nodes'][node_key]['pos']
-            node_names[node_key] = graph['nodes'][node_key]['name']
-            node_feature = np.concatenate((
-                utils.get_sentence_embedding(graph['nodes'][node_key]['name']),
-                graph['nodes'][node_key]['type']
-            ))
-            assert count == node_key
-            graph_nodes.append(node_feature)
-            node_color_list.append(utils.get_object_color_from_type(
-                graph['nodes'][node_key]['type']))
-
-        graph['node_coords'] = node_coords
-        graph['node_names'] = node_names
-        graph['graph_nodes'] = graph_nodes  # node features
-        src = []
-        dst = []
-        for edge in graph['edge_index']:
-            src.append(edge[0])
-            dst.append(edge[1])
-        graph['graph_edge_index'] = [src, dst]
-
-        graph['graph_image'] = utils.get_graph_image(
-            graph['edge_index'],
-            node_names, node_color_list
-        )
+        # Ensure graph connectivity
+        graph.ensure_connectivity(self.occupancy_grid)
 
         return graph
 
     def get_known_costs(self):
         known_cost = {'initial_robot_pose': {}}
-        init_r = self.get_robot_pose()
+        init_r = [self.robot_pose.x, self.robot_pose.y]
         cnt_ids = ['initial_robot_pose'] + [cnt['id'] for cnt in self.containers]
         cnt_positions = [init_r] + [cnt['position'] for cnt in self.containers]
 
@@ -372,3 +305,36 @@ class ThorInterface:
                 known_cost[cnt2_id][cnt1_id] = round(cost, 4)
 
         return known_cost
+
+    def get_top_down_image(self, orthographic=True):
+        if self.cached_data is not None:
+            if orthographic:
+                return self.cached_data['image_ortho']
+            else:
+                return self.cached_data['image_persp']
+        # Setup top down camera
+        event = self.controller.step(action="GetMapViewCameraProperties", raise_for_failure=True)
+        pose = copy.deepcopy(event.metadata["actionReturn"])
+
+        bounds = event.metadata["sceneBounds"]["size"]
+        max_bound = max(bounds["x"], bounds["z"])
+
+        pose["fieldOfView"] = 50
+        pose["position"]["y"] += 1.1 * max_bound
+        pose["orthographic"] = orthographic
+        pose["farClippingPlane"] = 50
+        if orthographic:
+            pose["orthographicSize"] = 0.5 * max_bound
+        else:
+            del pose["orthographicSize"]
+
+        # Add the camera to the scene
+        event = self.controller.step(
+            action="AddThirdPartyCamera",
+            **pose,
+            skyboxColor="white",
+            raise_for_failure=True,
+        )
+        top_down_image = event.third_party_camera_frames[-1]
+        top_down_image = top_down_image[::-1, ...]
+        return top_down_image
