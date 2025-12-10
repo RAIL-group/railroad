3e0e0ba5bd2a971e6b03a5efcdd3ad9822b9a313
Gregory Stein <gregory.j.stein@gmail.com>
2025-12-09 20:38:13 -0500

    base 'den prep' example working; couch single robot for now

--- mrppddl/include/mrppddl/constants.hpp
+++ mrppddl/include/mrppddl/constants.hpp
@@ -3,7 +3,7 @@
 namespace mrppddl {
 
 const double HEURISTIC_CANNOT_FIND_GOAL_PENALTY = 0.0;
-const double HEURISTIC_MULTIPLIER = 2;
+const double HEURISTIC_MULTIPLIER = 5;
 const double SUCCESS_REWARD = 0.0;
 const double ALL_ROBOTS_WAITING_PENALTY = 10.0;
 const int NUM_EXTRA_VISITS_PROB = 0;
