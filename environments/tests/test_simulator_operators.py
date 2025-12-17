bc43f3ee71151fcc66f32643fdba5414fa07bedc
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2025-12-17 16:28:01 -0500

    add non_blocking operator version in test

--- environments/tests/test_simulator_operators.py
+++ environments/tests/test_simulator_operators.py
@@ -13,7 +13,7 @@ def test_move_operator():
         "location": {"start", "roomA", "roomB", "roomC"},
     }
     move_time = lambda r, f, t: 10 if r == "r1" else 15  # noqa E731, E741
-    move_op = environments.operators.construct_move_operator(move_time=move_time)
+    move_op = environments.operators.construct_move_operator_nonblocking(move_time=move_time)
 
     initial_state = State(
         time=0,
@@ -132,8 +132,8 @@ def test_pick_and_place_operator():
             F("free", "r2"),
         },
     )
-    pick_op = environments.operators.construct_pick_operator(pick_time=pick_time)
-    place_op = environments.operators.construct_place_operator(place_time=place_time)
+    pick_op = environments.operators.construct_pick_operator_nonblocking(pick_time=pick_time)
+    place_op = environments.operators.construct_place_operator_nonblocking(place_time=place_time)
 
     pick_actions = pick_op.instantiate(objects_by_type)
     place_actions = place_op.instantiate(objects_by_type)
