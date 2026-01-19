e8012e198623dbf699633d52f8a86ae130c5ee5f
Gregory Stein <gregory.j.stein@gmail.com>
2026-01-19 11:26:23 -0500

    add more reasonable heuristic multiplier to 'more likely' test

--- packages/mrppddl/tests/test_planner.py
+++ packages/mrppddl/tests/test_planner.py
@@ -141,7 +141,7 @@ def test_mcts_search_picks_more_likely_location(roomA_prob, num_robots):
     selected_actions = []
     num_planning_attempts = 20
     for _ in range(num_planning_attempts):
-        action = mcts(initial_state, goal, max_iterations=10000, c=100)
+        action = mcts(initial_state, goal, max_iterations=10000, c=100, heuristic_multiplier=2)
         selected_actions.append(action)
 
     # Count how many selected actions mention roomA
