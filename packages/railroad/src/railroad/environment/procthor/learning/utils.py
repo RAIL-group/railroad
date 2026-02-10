b9241de9024f3e1aa7cfb30a18b006afe38aa14b
Abhishek Paudel <10024463+abpaudel@users.noreply.github.com>
2026-02-09 23:04:54 -0500

    refactor + improvements

--- packages/railroad/src/railroad/environment/procthor/learning/utils.py
+++ packages/railroad/src/railroad/environment/procthor/learning/utils.py
@@ -1,13 +1,12 @@
-
 from typing import Optional, Dict, List
-import os
+from pathlib import Path
 import numpy as np
 from ..resources import DEFAULT_RESOURCES_BASE, DEFAULT_SBERT_SUBDIR
 from ..scenegraph import SceneGraph
 
 
 def prepare_fcnn_input(
-    graph: SceneGraph,
+    graph: "SceneGraph",
     containers: List[int],
     objects_to_find: List[str]
 ) -> Dict[str, np.ndarray]:
@@ -16,8 +15,8 @@ def prepare_fcnn_input(
     objs_idx = []
     for obj in objects_to_find:
         idx = graph.add_node({
-            'name': obj,
-            'type': [0, 0, 0, 1],
+            "name": obj,
+            "type": [0, 0, 0, 1],
         })
         objs_idx.append(idx)
 
@@ -45,34 +44,28 @@ def compute_node_features(nodes: Dict[int, Dict[str, str]]) -> np.ndarray:
     features = []
     for node in nodes.values():
         node_feature = np.concatenate((
-            get_sentence_embedding(node['name']), node['type']
+            get_sentence_embedding(node["name"]), node["type"]
         ))
         features.append(node_feature)
     return np.array(features)
 
 
 def load_sentence_embedding(target_file_name: str) -> Optional[np.ndarray]:
-    target_dir = os.path.join(DEFAULT_RESOURCES_BASE, DEFAULT_SBERT_SUBDIR, 'cache')
-    if not os.path.exists(target_dir):
-        os.makedirs(target_dir)
-    # Walk through all directories and files in target_dir
-    for root, dirs, files in os.walk(target_dir):
-        if target_file_name in files:
-            file_path = os.path.join(root, target_file_name)
-            if os.path.exists(file_path):
-                return np.load(file_path)
+    target_dir = Path(DEFAULT_RESOURCES_BASE) / DEFAULT_SBERT_SUBDIR / "cache"
+    file_path = target_dir / target_file_name
+    if file_path.exists():
+        return np.load(file_path)
     return None
 
 
 def get_sentence_embedding(sentence: str) -> np.ndarray:
-    loaded_embedding = load_sentence_embedding(sentence + '.npy')
-    if loaded_embedding is None:
-        from sentence_transformers import SentenceTransformer
-        model_path = os.path.join(DEFAULT_RESOURCES_BASE, DEFAULT_SBERT_SUBDIR)
-        model = SentenceTransformer(model_path)
-        sentence_embedding = model.encode([sentence])[0]
-        file_name = os.path.join(model_path, 'cache', sentence + '.npy')
-        np.save(file_name, sentence_embedding)
-        return sentence_embedding
-    else:
+    loaded_embedding = load_sentence_embedding(f"{sentence}.npy")
+    if loaded_embedding is not None:
         return loaded_embedding
+    from sentence_transformers import SentenceTransformer
+    model_dir = Path(DEFAULT_RESOURCES_BASE) / DEFAULT_SBERT_SUBDIR
+    model = SentenceTransformer(model_dir.as_posix())
+    sentence_embedding = model.encode([sentence])[0]
+    file_path = model_dir / "cache" / f"{sentence}.npy"
+    np.save(file_path, sentence_embedding)
+    return sentence_embedding
